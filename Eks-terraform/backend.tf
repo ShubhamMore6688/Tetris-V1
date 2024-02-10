ff8c86c038c5701e0693c764575bc39be8fbc16c
Shubham More <94038708+ShubhamMore6688@users.noreply.github.com>
2024-02-10 08:01:36 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "terra-batch-5-remote-bucket-more" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
-    region = "eu-north-1"
-  }
-}
+# terraform {
+ # backend "s3" {
+  #  bucket = "terra-batch-5-remote-bucket-more" # Replace with your actual S3 bucket name
+   # key    = "Jenkins/terraform.tfstate"
+    #region = "eu-north-1"
+  # }
+# }
